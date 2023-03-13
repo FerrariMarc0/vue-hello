3f662e1b8e743b031247ee57890decbf51e88985
FerrariMarc0 <marco1010@live.it>
2023-03-13 17:59:11 +0100

    my first vue

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-'use strict';
+/* 'use strict'; */
 
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
-        
+        title: 'My first Vue',
+        imgs: [],
+        myClass: ['my_img', 'div_position', 'b_style', 'my_text'],
+        message: '...'
+      }
+    },
+    methods:{
+      showImg(){
+        this.imgs= ["img/01.jpg"];
+        this.message= '...Prova a prendermi';
       }
     }
-  }).mount('#app')
+  }).mount('#app') 
